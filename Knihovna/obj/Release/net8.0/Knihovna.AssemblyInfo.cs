d639de0532494d775fa627c1ce8797ebb8ec08d2
Pavel <pt75@seznam.cz>
2024-06-30 14:23:45 +0200

    obrazek na pozadi

--- Knihovna/obj/Release/net8.0/Knihovna.AssemblyInfo.cs
+++ Knihovna/obj/Release/net8.0/Knihovna.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Knihovna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c338edc6e94f3bb6c121c1f8fe33fd153ca290f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b01c6c1fb588e3ae133684be31e4931ca5e4dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Knihovna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Knihovna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
